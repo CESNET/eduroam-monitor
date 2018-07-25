635170122af80e44e265299b95d80702e60beaeb
lager <machv@cesnet.cz>
2018-07-25 09:25:51 +0200

    add header & comments, wrap code

--- generate_matrix.sh
+++ generate_matrix.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# =================================================================================
+# this script generates source data for eduroam availability matrix
+# it is intended to be run by cron at regular intervals
+# =================================================================================
 
-{ echo "host_name,service_description,service_state,service_last_check" ; icingacli monitoring list services --service="*@*" --format='$host_name$,$service_description$,$service_state$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check | /var/www/eduroam-monitor/sortcsvradius.py ; } | jq -R -s -f /var/www/eduroam-monitor/filter.jq -c -r > /var/www/eduroam-monitor/matrix/data.json
+{
+  echo "host_name,service_description,service_state,service_last_check" ;       # add csv header
+  icingacli monitoring list services --service="*@*" --format='$host_name$,$service_description$,$service_state$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check |  # get data
+  /var/www/eduroam-monitor/sortcsvradius.py ;                                   # sort domains
+} |
+  jq -R -s -f /var/www/eduroam-monitor/filter.jq -c -r > /var/www/eduroam-monitor/matrix/data.json      # convert to json & output to file
