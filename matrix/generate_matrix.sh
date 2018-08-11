a96d5f1c837cda1cf3e256e9930e2ad6f44359a1
lager <machv@cesnet.cz>
2018-08-11 22:14:30 +0200

    add current state type infomation to data

--- matrix/generate_matrix.sh
+++ matrix/generate_matrix.sh
@@ -5,8 +5,8 @@
 # =================================================================================
 
 {
-  echo "host_name,service_description,service_state,service_current_check_attempt,service_last_check" ;       # add csv header
-  icingacli monitoring list services --service="*@*" --format='$host_name$,$service_description$,$service_state$,$service_current_check_attempt$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check,service_current_check_attempt |  # get data
+  echo "host_name,service_description,service_state,service_current_check_attempt,service_state_type,service_last_check" ;       # add csv header
+  icingacli monitoring list services --service="*@*" --format='$host_name$,$service_description$,$service_state$,$service_current_check_attempt$,$service_state_type$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check,service_current_check_attempt,service_state_type |  # get data
   /var/www/eduroam-monitor/sortcsvradius.py ;                                   # sort domains
 } |
   jq -R -s -f /var/www/eduroam-monitor/filter.jq -c -r > /var/www/eduroam-monitor/matrix/data.json      # convert to json & output to file
