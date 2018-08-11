f840240372dfe8a22cd75d93b0a7c89000584cdf
lager <machv@cesnet.cz>
2018-08-11 23:08:20 +0200

    fix file paths

--- matrix/generate_matrix.sh
+++ matrix/generate_matrix.sh
@@ -7,6 +7,6 @@
 {
   echo "host_name,service_description,service_state,service_current_check_attempt,service_state_type,service_last_check" ;       # add csv header
   icingacli monitoring list services --service="*@*" --format='$host_name$,$service_description$,$service_state$,$service_current_check_attempt$,$service_state_type$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check,service_current_check_attempt,service_state_type |  # get data
-  /var/www/eduroam-monitor/sortcsvradius.py ;                                   # sort domains
+  /var/www/eduroam-monitor/matrix/sortcsvradius.py ;                                   # sort domains
 } |
-  jq -R -s -f /var/www/eduroam-monitor/filter.jq -c -r > /var/www/eduroam-monitor/matrix/data.json      # convert to json & output to file
+  jq -R -s -f /var/www/eduroam-monitor/matrix/filter.jq -c -r > /var/www/eduroam-monitor/matrix/html/data.json      # convert to json & output to file
