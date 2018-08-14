c89d9730914a5b76beb176c5424cf59ca9f7f22e
lager <machv@cesnet.cz>
2018-08-14 16:25:18 +0200

    remove service_current_check_attempt from data

--- matrix/generate_matrix.sh
+++ matrix/generate_matrix.sh
@@ -5,8 +5,8 @@
 # =================================================================================
 
 {
-  echo "host_name,service_description,service_state,service_current_check_attempt,service_state_type,service_last_check" ;       # add csv header
-  icingacli monitoring list services --service="*@*" --format='$host_name$,$service_description$,$service_state$,$service_current_check_attempt$,$service_state_type$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check,service_current_check_attempt,service_state_type |  # get data
+  echo "host_name,service_description,service_state,service_state_type,service_last_check" ;       # add csv header
+  icingacli monitoring list services --service="*@*" --format='$host_name$,$service_description$,$service_state$,$service_state_type$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check,service_state_type |  # get data
   /var/www/eduroam-monitor/matrix/sortcsvradius.py ;                                   # sort domains
 } |
   jq -R -s -f /var/www/eduroam-monitor/matrix/filter.jq -c -r > /var/www/eduroam-monitor/matrix/html/data.json      # convert to json & output to file
