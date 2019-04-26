b8993a3cf72096a0b18a5ce306b6bbc688225497
lager <machv@cesnet.cz>
2019-04-26 10:22:24 +0200

    service names are in form @*

--- matrix/generate_matrix.sh
+++ matrix/generate_matrix.sh
@@ -6,7 +6,7 @@
 
 {
   echo "host_name,service_description,service_state,service_state_type,service_last_check" ;       # add csv header
-  icingacli monitoring list services --service="*@*" --format='$host_name$,$service_description$,$service_state$,$service_state_type$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check,service_state_type |  # get data
+  icingacli monitoring list services --service="@*" --format='$host_name$,$service_description$,$service_state$,$service_state_type$,$service_last_check$' --columns=host_name,service_state,service_description,service_last_check,service_state_type |  # get data
   /var/www/eduroam-monitor/matrix/sortcsvradius.py ;                                   # sort domains
 } |
   jq -R -s -f /var/www/eduroam-monitor/matrix/filter.jq -c -r > /var/www/eduroam-monitor/matrix/html/data.json      # convert to json & output to file
