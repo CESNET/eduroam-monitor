1a37b68d45399d5ba5dac886b3c4b053b9a7af9c
lager <machv@cesnet.cz>
2019-05-02 23:38:00 +0200

    use dokuwiki format instead of plain html

--- ca_list/ca_list.py
+++ ca_list/ca_list.py
@@ -11,38 +11,18 @@ import cert_db
 
 lines = sys.stdin.readlines()
 
-print("<html>")
-print("<head>")
-print('<meta charset="UTF-8">')
-print("</head>")
-print("<table>")
-
-print("<tr>")
-print("<th>organizace</th>")
-print("<th>realmy</th>")
-print("<th>použitá CA</th>")
-print("<th>jména RADIUS serverů</th>")
-print("</tr>")
-
+# table header
+print("^ organizace ^ realmy ^ použitá CA ^ jména RADIUS serverů ^")
 
 for line in lines:
   input_array = line.split(";")
 
-  print("<tr>")
-
   # org name
-  print('<th><a href="' + input_array[0].split(",")[0] + '">' + ",".join(input_array[0].split(",")[1:]) + '</a></th>')
+  org_name = '[[' + input_array[0].split(",")[0] + '|' + ",".join(input_array[0].split(",")[1:]) + ']]'
 
   # realms
-  realms = input_array[1].split(",")
-  print('<th>')
-
-  for i in realms:
-    # TODO - add newline to table
-    print('@' + i)
-
-  print('</th>')
-    
+  #  # TODO - add newline to table
+  realms = ",".join(["@" + i for i in input_array[1].split(",")])
 
   # cert info, use only CN field
   cert_info = input_array[2].split(",")
@@ -51,23 +31,15 @@ for line in lines:
     if "CN=" in i:
       ca_name = re.sub("^\s*", "", i).replace("CN=", "")    # remove empty characters at the beginning and remove 'CN='
       if ca_name in cert_db.db:
-        print('<th><a href="' + cert_db.db[ca_name] + '">' + ca_name + '</a></th>')
+        cert_info = '[[' + cert_db.db[ca_name] + '|' + ca_name + ']]'
       else:
-        print('<th>' + ca_name + '</th>')
+        cert_info = ca_name
       break
 
   # radius servers
-  radius_servers = input_array[3].split(",")
-  print('<th>')
+  # TODO - add newline to table
+  radius_servers = input_array[3].replace('\n', '')
 
-  for i in radius_servers:
-    # TODO - add newline to table
-    print(i)
+  # final output
+  print('| ' + org_name + ' | ' + realms + ' | ' + cert_info + ' | ' + radius_servers + ' |')
 
-  print('</th>')
-
-
-  print("</tr>")
-
-
-print("</html>")
